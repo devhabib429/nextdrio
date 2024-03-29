1938cf0b4a9a9e357d546b5022fbbfcb38cdebea
Habibur <habibur@nestorbird.com>
2024-03-30 04:37:13 +0530

    Community Hero responsive

--- pages/Community/Communityhero.js
+++ pages/Community/Communityhero.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Link from 'next/link'; // Import Link from 'next/link'
-import '../../styles/Opensource.module.css';
+import Link from 'next/link';
 
 const Communityhero = () => {
     const [typedText, setTypedText] = useState('');
@@ -29,28 +28,29 @@ const Communityhero = () => {
                 <title>NextDrio</title>
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" />
             </Head>
-            <section className="p-4 h-screen sm:p-8 md:p-16 lg:p-20 xl:p-40 bg-black mx-auto flex flex-col-reverse md:flex-row items-center justify-between space-y-8 md:space-y-0 md:space-x-64">
-                <div className="w-full md:w-1/2 pt-48">
-                    <h2 className="text-2xl text-white md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4 md:mb-12">
+            <section className="h-screen p-4 md:p-8 lg:p-16 xl:p-20 bg-black flex flex-col md:flex-row items-center justify-center">
+                <div className="flex flex-col justify-center items-center md:items-start text-center md:text-left md:w-1/2 md:p-0 md:pr-8 lg:pr-16 xl:pr-20">
+                    <h2 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-white font-bold mb-4 md:mb-8">
                         {typedText}
                     </h2>
-                    <p className="text-base text-white font-poppins md:text-lg lg:text-lg xl:text-xl mb-12">
+                    <p className="text-base md:text-lg lg:text-lg xl:text-xl text-white font-poppins mb-8 md:max-w-lg">
                         Welcome to our vibrant community where collaboration fuels innovation, ideas are freely shared, and together we grow stronger.
                     </p>
-                    {/* Use Link component instead of <a> tag */}
                     <Link href="https://discord.com" passHref>
-                        <div className="bg-transparent border border-white text-white py-2 px-4 rounded-full inline-block hover:bg-white hover:text-black transition duration-300">
+                        <div className="bg-white text-black py-2 px-4 rounded-full inline-block hover:bg-black hover:text-white transition duration-300">
                             Join Discord
                         </div>
                     </Link>
                 </div>
-                <div className="w-full md:w-1/2 pt-16">
-                    <Image
-                        src="/image/communityhero.svg"
-                        alt="Image Description"
-                        width={500}
-                        height={500}
-                    />
+                <div className="flex justify-center items-center md:w-1/2">
+                    <div className="max-w-sm md:max-w-full">
+                        <Image
+                            src="/image/communityhero.svg"
+                            alt="Image Description"
+                            width={500}
+                            height={500}
+                        />
+                    </div>
                 </div>
             </section>
         </>
